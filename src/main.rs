afbd5b0787e074c3f58df3851ade3ee04ceb4da6
Joe <jmcook1186@gmail.com>
2022-05-10 17:17:42 +0100

    make request impl on BeaconClient struct

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,20 @@
 pub mod api_client;
 use serde_json;
+use tokio::main;
 
+#[tokio::main]
+async fn main() {
 
-fn main() {
+    let ip_addr = "localhost";
+    let port_id = "8002";
+    let endpoint = "eth/v1/node/version";
+    let state_id = "";
 
+    let url = format!("http://{}:{}/{}/{}", ip_addr, port_id, endpoint, state_id);
+    let client = api_client::BeaconClient::new(reqwest::Client::new());
+    
+    let version = client.get_node_version(ip_addr, port_id, endpoint, state_id).await;
 
-    let version = api_client::get_node_version().unwrap().to_string();
-
-    println!(
-        "version: {}", version
-    );
+    println!("{:?}", version.unwrap().to_string());
 
 }
