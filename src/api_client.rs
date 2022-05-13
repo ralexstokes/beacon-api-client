13ace7b8af670b8012c92ab2bc6bfbb75287b293
Joe <jmcook1186@gmail.com>
2022-05-13 10:20:51 +0100

    prints target url and response to terminal

--- src/api_client.rs
+++ src/api_client.rs
@@ -58,7 +58,11 @@ impl Client {
         &self,
         path: &str,
     ) -> Result<T, Error> {
+        
         let result: ApiResult<T> = self.http_get(path).await?.json().await?;
+        
+        println!("RESPONSE2: {:?}", serde_json::to_string(&result));
+        
         match result {
             ApiResult::Ok(result) => Ok(result),
             ApiResult::Err(err) => Err(err.into()),
@@ -67,7 +71,13 @@ impl Client {
 
     pub async fn http_get(&self, path: &str) -> Result<reqwest::Response, Error> {
         let target = self.endpoint.join(path)?;
+
+        println!("TARGET: {:?}\n\n", &target);
+
         let response = self.http.get(target).send().await?;
+        
+        println!("RESPONSE:  {:?}\n\n", &response);
+
         Ok(response)
     }
 
@@ -97,299 +107,301 @@ impl Client {
     }
 
     /* beacon namespace */
-    // pub async fn get_genesis_details(&self) -> Result<GenesisDetails, Error> {
-    //     self.get("/eth/v1/beacon/genesis").await
-    // }
-
-    // pub async fn get_state_root(id: StateId) -> Result<Root, Error> {
-    //     unimplemented!("")
-    // }
-
-    // pub async fn get_fork(id: StateId) -> Result<Fork, Error> {
-    //     unimplemented!("")
-    // }
-
-    // pub async fn get_finality_checkpoints(id: StateId) -> Result<FinalityCheckpoints, Error> {
-    //     unimplemented!("")
-    // }
-
-    // pub async fn get_validators(
-    //     id: StateId,
-    //     filters: &[ValidatorDescriptor],
-    // ) -> Result<Vec<ValidatorSummary>, Error> {
-    //     unimplemented!("")
-    // }
-
-    // pub async fn get_validator(
-    //     id: StateId,
-    //     validator_id: PubkeyOrIndex,
-    // ) -> Result<ValidatorSummary, Error> {
-    //     unimplemented!("")
-    // }
-
-    // pub async fn get_balances(
-    //     id: StateId,
-    //     filters: &[PubkeyOrIndex],
-    // ) -> Result<Vec<BalanceSummary>, Error> {
-    //     unimplemented!("")
-    // }
-
-    // pub async fn get_all_committees(id: StateId) -> Result<Vec<CommitteeSummary>, Error> {
-    //     unimplemented!("")
-    // }
-
-    // pub async fn get_committees(
-    //     id: StateId,
-    //     filter: CommitteeFilter,
-    // ) -> Result<Vec<CommitteeSummary>, Error> {
-    //     unimplemented!("")
-    // }
-
-    // pub async fn get_sync_committees(
-    //     id: StateId,
-    //     epoch: Option<Epoch>,
-    // ) -> Result<Vec<SyncCommitteeSummary>, Error> {
-    //     unimplemented!("")
-    // }
-
-    // pub async fn get_beacon_header_at_head() -> Result<BeaconHeaderSummary, Error> {
-    //     unimplemented!("")
-    // }
-
-    // pub async fn get_beacon_header_for_slot(slot: Slot) -> Result<BeaconHeaderSummary, Error> {
-    //     unimplemented!("")
-    // }
-
-    // pub async fn get_beacon_header_for_parent_root(
-    //     parent_root: Root,
-    // ) -> Result<BeaconHeaderSummary, Error> {
-    //     unimplemented!("")
-    // }
-
-    // pub async fn get_beacon_header(id: BlockId) -> Result<BeaconHeaderSummary, Error> {
-    //     unimplemented!("")
-    // }
-
-    // pub async fn post_signed_beacon_block(block: &SignedBeaconBlock) -> Result<(), Error> {
-    //     unimplemented!("")
-    // }
-
-    // pub async fn post_signed_blinded_beacon_block(
-    //     block: &SignedBlindedBeaconBlock,
-    // ) -> Result<(), Error> {
-    //     unimplemented!("")
-    // }
-
-    // // v2 endpoint
-    // pub async fn get_beacon_block(id: BlockId) -> Result<SignedBeaconBlock, Error> {
-    //     unimplemented!("")
-    // }
-
-    // pub async fn get_beacon_block_root(id: BlockId) -> Result<Root, Error> {
-    //     unimplemented!("")
-    // }
-
-    // pub async fn get_attestations_from_beacon_block(
-    //     id: BlockId,
-    // ) -> Result<Vec<Attestation>, Error> {
-    //     unimplemented!("")
-    // }
-
-    // pub async fn get_attestations_from_pool(
-    //     slot: Option<Slot>,
-    //     committee_index: Option<CommitteeIndex>,
-    // ) -> Result<Vec<Attestation>, Error> {
-    //     unimplemented!("")
-    // }
-
-    // pub async fn post_attestations(attestations: &[Attestation]) -> Result<(), Error> {
-    //     unimplemented!("")
-    // }
-
-    // pub async fn get_attester_slashings_from_pool() -> Result<Vec<AttesterSlashing>, Error> {
-    //     unimplemented!("")
-    // }
-
-    // pub async fn post_attester_slashing(attester_slashing: &AttesterSlashing) -> Result<(), Error> {
-    //     unimplemented!("")
-    // }
-
-    // pub async fn get_proposer_slashings_from_pool() -> Result<Vec<ProposerSlashing>, Error> {
-    //     unimplemented!("")
-    // }
-
-    // pub async fn post_proposer_slashing(proposer_slashing: &ProposerSlashing) -> Result<(), Error> {
-    //     unimplemented!("")
-    // }
-
-    // pub async fn post_sync_committee_messages(
-    //     messages: &[SyncCommitteeMessage],
-    // ) -> Result<(), Error> {
-    //     unimplemented!("")
-    // }
-
-    // pub async fn get_voluntary_exits_from_pool() -> Result<Vec<SignedVoluntaryExit>, Error> {
-    //     unimplemented!("")
-    // }
-
-    // pub async fn post_signed_voluntary_exit(exit: &SignedVoluntaryExit) -> Result<(), Error> {
-    //     unimplemented!("")
-    // }
-
-    // /* config namespace */
-    // pub async fn get_fork_schedule() -> Result<Vec<Fork>, Error> {
-    //     unimplemented!("")
-    // }
-
-    // pub async fn get_spec() -> Result<HashMap<String, String>, Error> {
-    //     unimplemented!("")
-    // }
-
-    // pub async fn get_deposit_contract_address() -> Result<(ChainId, ExecutionAddress), Error> {
-    //     unimplemented!("")
-    // }
-
-    // /* debug namespace */
-    // // v2 endpoint
-    // pub async fn get_state(id: StateId) -> Result<BeaconState, Error> {
-    //     unimplemented!("")
-    // }
-
-    // // v2 endpoint
-    // pub async fn get_heads() -> Result<Vec<Coordinate>, Error> {
-    //     unimplemented!("")
-    // }
-
-    // /* events namespace */
-    // // TODO: figure out return type
-    // pub async fn get_events<T>(topics: &[EventTopic]) -> Result<T, Error> {
-    //     // get back "event: TOPIC, data: T"
-    //     unimplemented!("")
-    // }
-
-    // /* node namespace */
-    // pub async fn get_node_identity() -> Result<NetworkIdentity, Error> {
-    //     unimplemented!("")
-    // }
-
-    // pub async fn get_node_peers(filters: &[PeerDescriptor]) -> Result<Vec<PeerDescription>, Error> {
-    //     unimplemented!("")
-    // }
-
-    // pub async fn get_peer(peer_id: Multiaddr) -> Result<PeerDescription, Error> {
-    //     unimplemented!("")
-    // }
-
-    // pub async fn get_peer_count() -> Result<PeerSummary, Error> {
-    //     unimplemented!("")
-    // }
-
-    pub async fn get_node_version(&self) -> ApiResult<String> {
-        
-        self.get("/eth/v1/node/version/").await
+    pub async fn get_genesis_details(&self) -> Result<GenesisDetails, Error> {
+        self.get("/eth/v1/beacon/genesis").await
     }
 
-    // pub async fn get_sync_status() -> Result<SyncStatus, Error> {
-    //     unimplemented!("")
-    // }
+    pub async fn get_state_root(id: StateId) -> Result<Root, Error> {
+        unimplemented!("")
+    }
 
-    // pub async fn get_health() -> Result<HealthStatus, Error> {
-    //     unimplemented!("")
-    // }
+    pub async fn get_fork(id: StateId) -> Result<Fork, Error> {
+        unimplemented!("")
+    }
 
-    // /* validator namespace */
-    // pub async fn get_attester_duties(
-    //     epoch: Epoch,
-    //     indices: &[ValidatorIndex],
-    // ) -> Result<(Root, Vec<AttestationDuty>), Error> {
-    //     unimplemented!("")
-    // }
+    pub async fn get_finality_checkpoints(id: StateId) -> Result<FinalityCheckpoints, Error> {
+        unimplemented!("")
+    }
 
-    // pub async fn get_proposer_duties(epoch: Epoch) -> Result<(Root, Vec<ProposerDuty>), Error> {
-    //     unimplemented!("")
-    // }
+    pub async fn get_validators(
+        id: StateId,
+        filters: &[ValidatorDescriptor],
+    ) -> Result<Vec<ValidatorSummary>, Error> {
+        unimplemented!("")
+    }
 
-    // pub async fn get_sync_committee_duties(
-    //     epoch: Epoch,
-    //     indices: &[ValidatorIndex],
-    // ) -> Result<(Root, Vec<SyncCommitteeDuty>), Error> {
-    //     unimplemented!("")
-    // }
+    pub async fn get_validator(
+        id: StateId,
+        validator_id: PubkeyOrIndex,
+    ) -> Result<ValidatorSummary, Error> {
+        unimplemented!("")
+    }
 
-    // // v2 endpoint
-    // pub async fn get_block(
-    //     slot: Slot,
-    //     randao_reveal: RandaoReveal,
-    //     graffiti: Bytes32,
-    // ) -> Result<BeaconBlock, Error> {
-    //     unimplemented!("")
-    // }
+    pub async fn get_balances(
+        id: StateId,
+        filters: &[PubkeyOrIndex],
+    ) -> Result<Vec<BalanceSummary>, Error> {
+        unimplemented!("")
+    }
 
-    // pub async fn get_blinded_block(
-    //     slot: Slot,
-    //     randao_reveal: RandaoReveal,
-    //     graffiti: Bytes32,
-    // ) -> Result<BlindedBeaconBlock, Error> {
-    //     unimplemented!("")
-    // }
+    pub async fn get_all_committees(id: StateId) -> Result<Vec<CommitteeSummary>, Error> {
+        unimplemented!("")
+    }
 
-    // pub async fn get_attestation_data(
-    //     slot: Slot,
-    //     committee_index: CommitteeIndex,
-    // ) -> Result<AttestationData, Error> {
-    //     unimplemented!("")
-    // }
+    pub async fn get_committees(
+        id: StateId,
+        filter: CommitteeFilter,
+    ) -> Result<Vec<CommitteeSummary>, Error> {
+        unimplemented!("")
+    }
 
-    // pub async fn get_attestation_aggregate(
-    //     attestation_data_root: Root,
-    //     slot: Slot,
-    // ) -> Result<Attestation, Error> {
-    //     unimplemented!("")
-    // }
+    pub async fn get_sync_committees(
+        id: StateId,
+        epoch: Option<Epoch>,
+    ) -> Result<Vec<SyncCommitteeSummary>, Error> {
+        unimplemented!("")
+    }
 
-    // pub async fn post_aggregates_with_proofs(
-    //     aggregates_with_proofs: &[SignedAggregateAndProof],
-    // ) -> Result<(), Error> {
-    //     Ok(())
-    // }
+    pub async fn get_beacon_header_at_head() -> Result<BeaconHeaderSummary, Error> {
+        unimplemented!("")
+    }
 
-    // pub async fn subscribe_subnets_for_attestation(
-    //     committee_descriptors: &[CommitteeDescriptor],
-    // ) -> Result<(), Error> {
-    //     Ok(())
-    // }
+    pub async fn get_beacon_header_for_slot(slot: Slot) -> Result<BeaconHeaderSummary, Error> {
+        unimplemented!("")
+    }
 
-    // pub async fn subscribe_subnets_for_sync_committee(
-    //     sync_committee_descriptors: &[SyncCommitteeDescriptor],
-    // ) -> Result<(), Error> {
-    //     Ok(())
-    // }
+    pub async fn get_beacon_header_for_parent_root(
+        parent_root: Root,
+    ) -> Result<BeaconHeaderSummary, Error> {
+        unimplemented!("")
+    }
 
-    // pub async fn get_sync_committee_contribution(
-    //     slot: Slot,
-    //     subcommittee_index: usize,
-    //     beacon_block_root: Root,
-    // ) -> Result<SyncCommitteeContribution, Error> {
-    //     unimplemented!("")
-    // }
+    pub async fn get_beacon_header(id: BlockId) -> Result<BeaconHeaderSummary, Error> {
+        unimplemented!("")
+    }
 
-    // pub async fn post_contributions_with_proofs(
-    //     contributions_with_proofs: &[SignedContributionAndProof],
-    // ) -> Result<(), Error> {
-    //     Ok(())
-    // }
+    pub async fn post_signed_beacon_block(block: &SignedBeaconBlock) -> Result<(), Error> {
+        unimplemented!("")
+    }
 
-    // pub async fn prepare_proposers(
-    //     registrations: &[BeaconProposerRegistration],
-    // ) -> Result<(), Error> {
-    //     Ok(())
-    // }
+    pub async fn post_signed_blinded_beacon_block(
+        block: &SignedBlindedBeaconBlock,
+    ) -> Result<(), Error> {
+        unimplemented!("")
+    }
 
-    // // endpoint for builder registrations
-    // pub async fn register_validators_with_builders(
-    //     registrations: &[SignedValidatorRegistration],
-    // ) -> Result<(), Error> {
-    //     Ok(())
-    // }
+    // v2 endpoint
+    pub async fn get_beacon_block(id: BlockId) -> Result<SignedBeaconBlock, Error> {
+        unimplemented!("")
+    }
+
+    pub async fn get_beacon_block_root(id: BlockId) -> Result<Root, Error> {
+        unimplemented!("")
+    }
+
+    pub async fn get_attestations_from_beacon_block(
+        id: BlockId,
+    ) -> Result<Vec<Attestation>, Error> {
+        unimplemented!("")
+    }
+
+    pub async fn get_attestations_from_pool(
+        slot: Option<Slot>,
+        committee_index: Option<CommitteeIndex>,
+    ) -> Result<Vec<Attestation>, Error> {
+        unimplemented!("")
+    }
+
+    pub async fn post_attestations(attestations: &[Attestation]) -> Result<(), Error> {
+        unimplemented!("")
+    }
+
+    pub async fn get_attester_slashings_from_pool() -> Result<Vec<AttesterSlashing>, Error> {
+        unimplemented!("")
+    }
+
+    pub async fn post_attester_slashing(attester_slashing: &AttesterSlashing) -> Result<(), Error> {
+        unimplemented!("")
+    }
+
+    pub async fn get_proposer_slashings_from_pool() -> Result<Vec<ProposerSlashing>, Error> {
+        unimplemented!("")
+    }
+
+    pub async fn post_proposer_slashing(proposer_slashing: &ProposerSlashing) -> Result<(), Error> {
+        unimplemented!("")
+    }
+
+    pub async fn post_sync_committee_messages(
+        messages: &[SyncCommitteeMessage],
+    ) -> Result<(), Error> {
+        unimplemented!("")
+    }
+
+    pub async fn get_voluntary_exits_from_pool() -> Result<Vec<SignedVoluntaryExit>, Error> {
+        unimplemented!("")
+    }
+
+    pub async fn post_signed_voluntary_exit(exit: &SignedVoluntaryExit) -> Result<(), Error> {
+        unimplemented!("")
+    }
+
+    /* config namespace */
+    pub async fn get_fork_schedule() -> Result<Vec<Fork>, Error> {
+        unimplemented!("")
+    }
+
+    pub async fn get_spec() -> Result<HashMap<String, String>, Error> {
+        unimplemented!("")
+    }
+
+    pub async fn get_deposit_contract_address() -> Result<(ChainId, ExecutionAddress), Error> {
+        unimplemented!("")
+    }
+
+    /* debug namespace */
+    // v2 endpoint
+    pub async fn get_state(id: StateId) -> Result<BeaconState, Error> {
+        unimplemented!("")
+    }
+
+    // v2 endpoint
+    pub async fn get_heads() -> Result<Vec<Coordinate>, Error> {
+        unimplemented!("")
+    }
+
+    /* events namespace */
+    // TODO: figure out return type
+    pub async fn get_events<T>(topics: &[EventTopic]) -> Result<T, Error> {
+        // get back "event: TOPIC, data: T"
+        unimplemented!("")
+    }
+
+    /* node namespace */
+    pub async fn get_node_identity() -> Result<NetworkIdentity, Error> {
+        unimplemented!("")
+    }
+
+    pub async fn get_node_peers(filters: &[PeerDescriptor]) -> Result<Vec<PeerDescription>, Error> {
+        unimplemented!("")
+    }
+
+    pub async fn get_peer(peer_id: Multiaddr) -> Result<PeerDescription, Error> {
+        unimplemented!("")
+    }
+
+    pub async fn get_peer_count() -> Result<PeerSummary, Error> {
+        unimplemented!("")
+    }
+
+    pub async fn get_node_version(&self) -> Result<String, Error> {
+        
+        let result = self.get::<String>(&"eth/v1/node/version").await.unwrap();
+        Ok(result)
+
+    }
+
+    pub async fn get_sync_status() -> Result<SyncStatus, Error> {
+        unimplemented!("")
+    }
+
+    pub async fn get_health() -> Result<HealthStatus, Error> {
+        unimplemented!("")
+    }
+
+    /* validator namespace */
+    pub async fn get_attester_duties(
+        epoch: Epoch,
+        indices: &[ValidatorIndex],
+    ) -> Result<(Root, Vec<AttestationDuty>), Error> {
+        unimplemented!("")
+    }
+
+    pub async fn get_proposer_duties(epoch: Epoch) -> Result<(Root, Vec<ProposerDuty>), Error> {
+        unimplemented!("")
+    }
+
+    pub async fn get_sync_committee_duties(
+        epoch: Epoch,
+        indices: &[ValidatorIndex],
+    ) -> Result<(Root, Vec<SyncCommitteeDuty>), Error> {
+        unimplemented!("")
+    }
+
+    // v2 endpoint
+    pub async fn get_block(
+        slot: Slot,
+        randao_reveal: RandaoReveal,
+        graffiti: Bytes32,
+    ) -> Result<BeaconBlock, Error> {
+        unimplemented!("")
+    }
+
+    pub async fn get_blinded_block(
+        slot: Slot,
+        randao_reveal: RandaoReveal,
+        graffiti: Bytes32,
+    ) -> Result<BlindedBeaconBlock, Error> {
+        unimplemented!("")
+    }
+
+    pub async fn get_attestation_data(
+        slot: Slot,
+        committee_index: CommitteeIndex,
+    ) -> Result<AttestationData, Error> {
+        unimplemented!("")
+    }
+
+    pub async fn get_attestation_aggregate(
+        attestation_data_root: Root,
+        slot: Slot,
+    ) -> Result<Attestation, Error> {
+        unimplemented!("")
+    }
+
+    pub async fn post_aggregates_with_proofs(
+        aggregates_with_proofs: &[SignedAggregateAndProof],
+    ) -> Result<(), Error> {
+        Ok(())
+    }
+
+    pub async fn subscribe_subnets_for_attestation(
+        committee_descriptors: &[CommitteeDescriptor],
+    ) -> Result<(), Error> {
+        Ok(())
+    }
+
+    pub async fn subscribe_subnets_for_sync_committee(
+        sync_committee_descriptors: &[SyncCommitteeDescriptor],
+    ) -> Result<(), Error> {
+        Ok(())
+    }
+
+    pub async fn get_sync_committee_contribution(
+        slot: Slot,
+        subcommittee_index: usize,
+        beacon_block_root: Root,
+    ) -> Result<SyncCommitteeContribution, Error> {
+        unimplemented!("")
+    }
+
+    pub async fn post_contributions_with_proofs(
+        contributions_with_proofs: &[SignedContributionAndProof],
+    ) -> Result<(), Error> {
+        Ok(())
+    }
+
+    pub async fn prepare_proposers(
+        registrations: &[BeaconProposerRegistration],
+    ) -> Result<(), Error> {
+        Ok(())
+    }
+
+    // endpoint for builder registrations
+    pub async fn register_validators_with_builders(
+        registrations: &[SignedValidatorRegistration],
+    ) -> Result<(), Error> {
+        Ok(())
+    }
 }
 
--- src/main.rs
+++ src/main.rs
@@ -5,7 +5,7 @@ pub mod serde;
 use tokio::main;
 use api_client::Client as Client;
 use url::{ParseError, Url};
-use types::ApiResult;
+
 
 #[tokio::main]
 async fn main() {
@@ -13,8 +13,8 @@ async fn main() {
     let s = "http://127.0.0.1:8003/";
     let url: Url = Url::parse(s).unwrap();
     let client = Client::new(url);
-    let result = serde_json::to_string(&client.get_node_version().await);
 
-    println!("{:?}", &result);
+    client.get_node_version().await;
+
 
 }
