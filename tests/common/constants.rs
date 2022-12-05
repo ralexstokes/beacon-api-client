e996893989385e002018991b63e8c66a94036b55
Perelyn <pisckid344@gmail.com>
2022-12-05 08:22:25 +0100

    Add constants

---
+++ tests/common/constants.rs
@@ -0,0 +1,25 @@
+pub const GENESIS_TIME: u64 = 1606824023;
+pub const GENESIS_STATE_ROOT: [u8; 32] = [
+    126, 118, 136, 14, 182, 123, 189, 200, 98, 80, 170, 87, 137, 88, 233, 208, 103, 94, 100, 231,
+    20, 51, 120, 85, 32, 79, 181, 171, 170, 248, 44, 43,
+];
+pub const GENESIS_FORK_VERSION: u32 = 0x00000000;
+pub const VALIDATOR_0 :&str  =
+"0x933ad9491b62059dd065b560d256d8957a8c402cc6e8d8ee7290ae11e8f7329267a8811c397529dac52ae1342ba58c95";
+
+// pub const genesis_block_root = 0xeade62f0457b2fdf48e7d3fc4b60736688286be7c7a3ac4c9a16a5e0600bd9e4
+// genesis_validators_root = 0x4b363db94e286120d76eb905340fdd4e54bfe9f06bf33ff6cf5ad27f511bfe95
+// genesis_validators_count = 21063
+// genesis_active_validators_count = 21063
+// genesis_total_active_stake_gwei = 674016000000000
+// genesis_total_balance_gwei = 674144000000000
+// eth1_data =
+// deposit_root = 0x1a4c3cce02935defd159e4e207890ae26a325bf03e205c9ee94ca040ecce008a
+// deposit_count = 21073
+// block_hash = 0xeeea1373d4aa9e099d7c9deddb694db9aeb4577755ef83f9b6345ce4357d9abf
+// GENESIS_FORK_VERSION = 0x00000000
+// genesis_fork_digest = 0xb5303f2a
+// pre_genesis_fork_digest = 0xf5a5fd42
+// pub const ONE_HUNDRED_PERCENT_BP = u64 = 10000;
+//
+// pub const GENESIS_TIME =
