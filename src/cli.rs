7b96122a059e108a9a67df99b7269116eabf17b5
Joe <jmcook1186@gmail.com>
2023-01-18 13:40:25 +0000

    initial commit for CLI

---
+++ src/cli.rs
@@ -0,0 +1,59 @@
+use crate::{CliArgs, Client, GenesisDetails, StateId};
+use ethereum_consensus::primitives::{Root, Slot};
+use std::str::Bytes;
+use url::Url;
+
+pub async fn parseCli(args: CliArgs) {
+    //set up client
+    let s = args.node_url;
+    let url: Url = Url::parse(&s).unwrap();
+    let client = Client::new(url);
+    let endpoint = args.endpoint.as_str();
+
+    match endpoint {
+        // Match a single value
+        "node_version" => println!("{:?}", client.get_node_version().await.unwrap()),
+        "genesis" => {
+            let out: GenesisDetails = client.get_genesis_details().await.unwrap();
+            println!("{:?}", out.genesis_time);
+            println!("{:?}", out.genesis_fork_version);
+            println!("{:?}", out.genesis_validators_root);
+        }
+        "root" => {
+            let state: StateId;
+            if args.payload.contains("finalized")
+                | args.payload.contains("Finalized")
+                | args.payload.contains("FINALIZED")
+            {
+                state = StateId::Finalized;
+            } else if args.payload.contains("justified")
+                | args.payload.contains("Justified")
+                | args.payload.contains("JUSTIFIED")
+            {
+                state = StateId::Justified;
+            } else if args.payload.contains("genesis")
+                | args.payload.contains("Genesis")
+                | args.payload.contains("GENESIS")
+            {
+                state = StateId::Genesis;
+            } else if args.payload.contains("0x") {
+                assert_eq!(args.payload.as_bytes().len(), 32, "malformed root in request payload");
+                let bytes: [u8; 32] = args.payload.as_bytes().try_into().unwrap();
+                state = StateId::Root(Root::from_bytes(bytes));
+            } else {
+                let check_numeric = args.payload.trim().parse::<u64>();
+                match check_numeric {
+                    Ok(ok) => state = StateId::Slot(check_numeric.unwrap()),
+                    Err(e) => {
+                        println!("error in request payload: please check formats");
+                        state = StateId::Slot(0)
+                    }
+                }
+            }
+            let out: Root = client.get_state_root(state).await.unwrap();
+            println!("{:?}", out);
+        }
+
+        _ => println!("something else"),
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,9 @@
-use beacon_api_client::{Client, StateId};
+use beacon_api_client::{parseCli, CliArgs, Client, StateId};
+use clap::Parser;
 use url::Url;
 
 #[tokio::main]
 async fn main() {
-    let s = "http://127.0.0.1:8003/";
-    let url: Url = Url::parse(s).unwrap();
-    let client = Client::new(url);
-
-    let checkpoints = client.get_finality_checkpoints(StateId::Finalized).await.unwrap();
-
-    println!("previous checkpoint: {:?}", checkpoints.previous_justified);
-    println!("current checkpoint: {:?}", checkpoints.current_justified);
-    println!("finalized checkpoint: {:?}", checkpoints.finalized);
+    let args = CliArgs::parse();
+    parseCli(args).await;
 }
