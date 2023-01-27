a9f3be3a99a84e66b90342464ec830345d31fa1c
Joe <jmcook1186@gmail.com>
2023-01-27 15:19:31 +0000

    add validator ns to ns-method check

--- src/cli.rs
+++ src/cli.rs
@@ -3,6 +3,7 @@ use url::Url;
 
 pub async fn run_cli(args: &Config) {
     parse_args(args);
+    // check_args_for_method(&args);
     let out = call_api(args).await;
     println!("{out:?}");
 }
@@ -32,8 +33,8 @@ pub fn check_method_in_ns(method: &Method, namespace: &Namespace) -> bool {
                     Method::Header |
                     Method::Headers |
                     Method::Block |
-                    Method::Blocks |
-                    Method::BlindedBlocks |
+                    Method::PostBlock |
+                    Method::PostBlindedBlock |
                     Method::BlockRoot |
                     Method::BlockAttestations |
                     Method::PoolAttestations |
@@ -64,9 +65,103 @@ pub fn check_method_in_ns(method: &Method, namespace: &Namespace) -> bool {
                     Method::Health
             )
         }
+        Namespace::Validator => {
+            matches!(
+                method,
+                Method::GetAttesterDuties |
+                    Method::GetProposerDuties |
+                    Method::GetSyncCommitteeDuties |
+                    Method::GetBlockproposal |
+                    Method::GetBlindedBlockProposal |
+                    Method::GetAttestationData |
+                    Method::GetAttestationAggregate |
+                    Method::PostAggregatesWithProofs |
+                    Method::SubscribeSubnetsForSyncCommittees |
+                    Method::GetSyncCommitteeContribution |
+                    Method::PostSyncCommitteeContributionWithProofs |
+                    Method::PrepareProposers |
+                    Method::RegisterValidatorsWithBuilders,
+            )
+        }
     };
     res
 }
+
+// pub fn check_args_for_method(args: &Config) {
+//     // set 1 contains methods requiring no args
+//     let no_args = vec![
+//         &Method::Genesis,
+//         &Method::Headers,
+//         &Method::PoolAttestations,
+//         &Method::AttesterSlashing,
+//         &Method::ProposerSlashing,
+//         &Method::VoluntaryExits,
+//         &Method::ForkSchedule,
+//         &Method::Spec,
+//         &Method::DepositContract,
+//         &Method::Head,
+//         &Method::Identity,
+//         &Method::Peers,
+//         &Method::PeerCount,
+//         &Method::Version,
+//         &Method::Syncing,
+//         &Method::Health,
+//     ];
+//     // set 2 contains methods requiring state_id
+//     let requires_state_id = vec![
+//         &Method::Fork,
+//         &Method::State,
+//         &Method::FinalityCheckpoints,
+//         &Method::Validators,
+//         &Method::ValidatorBalances,
+//         &Method::Committees,
+//         &Method::SyncCommittees,
+//         &Method::State,
+//     ];
+//     //set 3 requires some json payload
+//     let requires_payload = vec![
+//         &Method::PostBlock,
+//         &Method::PostBlindedBlock,
+//         &Method::PostAttestations,
+//         &Method::PostAttesterSlashing,
+//         &Method::PostSyncCommittees,
+//         &Method::PostVoluntaryExits,
+//     ];
+//     let method = &args.method;
+
+//     if no_args.contains(&method) {
+//         println!("no required arguments required for the selected method")
+//     }
+//     if requires_state_id.contains(&method) {
+//         assert!(args.state_id.is_some(), "state_id required for this method");
+//         println!("checked args against method: is ok");
+//     }
+//     if method == &Method::Validator | {
+//         assert!(
+//             args.state_id.is_some() && args.hex_string.is_some(),
+//             "state_id AND validator_id required for this method"
+//         );
+//         println!("checked args against method: is ok");
+//     }
+//     if method == &Method::Events {
+//         assert!(args.list_events.is_some(), "events subscriptions required for this method")
+//     }
+//     if method == &Method::Peer {
+//         assert!(args.id_peers.is_some(), "peer_id is required for this method")
+//     }
+//     if (method == &Method::Block) |
+//         (method == &Method::Header) |
+//         (method == &Method::BlockRoot) |
+//         (method == &Method::BlockAttestations)
+//     {
+//         assert!(args.block_id.is_some(), "block_id required for this method");
+//         println!("checked args against method: is ok");
+//     }
+//     if requires_payload.contains(&method) {
+//         assert!(args.payload.is_some(), "payload is required for this method");
+//         println!("checked args against method: is ok");
+//     }
+// }
 
 pub async fn call_api(args: &Config) -> String {
     //set up client
