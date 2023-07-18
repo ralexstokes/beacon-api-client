2b034b936b8f6d70a13f717303e2d5185edc6da7
Joe <jmcook1186@gmail.com>
2023-07-18 16:21:06 +0100

    fmt

--- src/cli/mod.rs
+++ src/cli/mod.rs
@@ -1,13 +1,14 @@
 mod config;
-use crate::mainnet::Client;
+use crate::{mainnet::Client, Error};
 pub use config::CliConfig;
 use config::{BeaconMethod, Namespace::Beacon};
-use crate::Error;
 use std::fmt;
 
-pub async fn run_cli(client: &Client, args: &CliConfig) -> Box<dyn fmt::Debug>{
+pub async fn run_cli(client: &Client, args: &CliConfig) -> Box<dyn fmt::Debug> {
     match &args.namespace {
         Beacon(BeaconMethod::Genesis) => Box::new(client.get_genesis_details().await),
-        Beacon(BeaconMethod::Root(arg)) => Box::new(client.get_state_root(arg.state_id.clone()).await)
+        Beacon(BeaconMethod::Root(arg)) => {
+            Box::new(client.get_state_root(arg.state_id.clone()).await)
+        }
     }
 }
