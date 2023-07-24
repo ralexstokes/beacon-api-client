f07f1f3a9fdcd23900510152efa6c66032d53b2b
Joe <jmcook1186@gmail.com>
2023-07-24 09:59:19 +0100

    fix clippy

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use beacon_api_client::{mainnet::Client, run_cli, CliConfig, Error};
+use beacon_api_client::{mainnet::Client, run_cli, CliConfig};
 use clap::Parser;
 use url::Url;
 
@@ -7,6 +7,6 @@ async fn main() -> Result<(), beacon_api_client::Error> {
     let args = CliConfig::parse();
     let url = Url::parse(&args.endpoint).unwrap();
     let client = Client::new(url);
-    dbg!(run_cli(&client, &args).await)?;
+    dbg!(run_cli(&client, &args).await?);
     Ok(())
 }
