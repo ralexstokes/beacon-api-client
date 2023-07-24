a8a74917a444ea39585ec6e4a86e7f3e18e20387
Joe <jmcook1186@gmail.com>
2023-07-24 09:52:46 +0100

    improve error handling

--- src/cli/mod.rs
+++ src/cli/mod.rs
@@ -1,28 +1,32 @@
 mod config;
-use crate::mainnet::Client;
+use crate::{mainnet::Client, Error};
 pub use config::CliConfig;
 use config::{BeaconMethod, Namespace::Beacon};
 use std::fmt;
 
-use self::config::StateIdArg;
-
-pub async fn run_cli(client: &Client, args: &CliConfig) -> Box<dyn fmt::Debug> {
+pub async fn run_cli(client: &Client, args: &CliConfig) -> Result<Box<dyn fmt::Debug>, Error> {
     match &args.namespace {
-        Beacon(BeaconMethod::Genesis) => Box::new(client.get_genesis_details().await),
+        Beacon(BeaconMethod::Genesis) => {
+            let result = client.get_genesis_details().await?;
+            Ok(Box::new(result))
+        }
         Beacon(BeaconMethod::Root(arg)) => {
-            Box::new(client.get_state_root(arg.state_id.clone()).await)
+            let result = client.get_state_root(arg.state_id.clone()).await?;
+            Ok(Box::new(result))
         }
         Beacon(BeaconMethod::FinalityCheckpoints(arg)) => {
-            Box::new(client.get_finality_checkpoints(arg.state_id.clone()).await)
+            let result = client.get_finality_checkpoints(arg.state_id.clone()).await?;
+            Ok(Box::new(result))
         }
-        Beacon(BeaconMethod::Validators(arg)) => Box::new(
-            client
+        Beacon(BeaconMethod::Validators(arg)) => {
+            let result = client
                 .get_validators(
                     arg.state_id.to_owned(),
                     &[arg.validator_id.to_owned()],
                     &[arg.status],
                 )
-                .await,
-        ),
+                .await?;
+            Ok(Box::new(result))
+        }
     }
 }
